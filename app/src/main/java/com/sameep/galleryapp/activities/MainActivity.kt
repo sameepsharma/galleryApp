861bb2a59350465361ee73c9ae0944798b1a002f
Neena Mishra <neena.infowind@gmail.com>
2020-04-06 14:42:09 +0530

    Show data in Grid Layout

---
+++ app/src/main/java/com/sameep/galleryapp/activities/MainActivity.kt
@@ -0,0 +1,174 @@
+package com.sameep.galleryapp.activities
+
+import android.Manifest
+import android.app.ProgressDialog
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sameep.galleryapp.R
+import com.sameep.galleryapp.adapters.GalleryAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NullPointerException
+
+
+private const val REQUEST_PERMISSIONS = 1001
+
+private var count = 0
+private lateinit var thumbnails: Array<Bitmap?>
+private var thumbnailsselection: BooleanArray? = null
+private lateinit var arrPath: Array<String?>
+private var typeMedia: IntArray? = null
+private var galleryAdapter: GalleryAdapter? = null
+lateinit var loader: ProgressBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViews()
+
+    }
+
+    private fun setupViews() {
+       main_loader.visibility=View.VISIBLE
+
+        val  layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
+        main_rv.layoutManager=layoutManager
+        fn_checkpermission()
+
+
+    }
+
+    private fun fn_checkpermission() {
+        /*RUN TIME PERMISSIONS*/
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) && ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
+                    REQUEST_PERMISSIONS
+                )
+            }
+        } else {
+            Log.e("Else", "Else")
+            getMedia()
+        }
+    }
+
+
+
+    private fun getMedia() {
+
+        val columns = arrayOf(
+            MediaStore.Files.FileColumns._ID,
+            MediaStore.Files.FileColumns.DATA,
+            MediaStore.Files.FileColumns.DATE_ADDED,
+            MediaStore.Files.FileColumns.MEDIA_TYPE,
+            MediaStore.Files.FileColumns.MIME_TYPE,
+            MediaStore.Files.FileColumns.TITLE
+        )
+        val selection = (MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)
+        val orderBy = MediaStore.Files.FileColumns.DATE_ADDED
+        val queryUri: Uri = MediaStore.Files.getContentUri("external")
+
+        val imagecursor: Cursor = this.managedQuery(
+            queryUri,
+            columns,
+            selection,
+            null,  // Selection args (none).
+            MediaStore.Files.FileColumns.DATE_ADDED + " DESC" // Sort order.
+        )
+
+        val image_column_index: Int = imagecursor.getColumnIndex(MediaStore.Files.FileColumns._ID)
+        count = imagecursor.getCount()
+        thumbnails = arrayOfNulls<Bitmap>(count)
+        arrPath = arrayOfNulls<String>(count)
+        typeMedia = IntArray(count)
+        thumbnailsselection = BooleanArray(count)
+        for (i in 0 until count) {
+            imagecursor.moveToPosition(i)
+            val id: Int = imagecursor.getInt(image_column_index)
+            val dataColumnIndex: Int = imagecursor.getColumnIndex(MediaStore.Files.FileColumns.DATA)
+            val bmOptions = BitmapFactory.Options()
+            bmOptions.inSampleSize = 4
+            bmOptions.inPurgeable = true
+            val type: Int = imagecursor.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
+            val t: Int = imagecursor.getInt(type)
+            if (t == 1) thumbnails[i] = MediaStore.Images.Thumbnails.getThumbnail(
+                this.getContentResolver(), id.toLong(),
+                MediaStore.Images.Thumbnails.MINI_KIND, bmOptions
+            ) else if (t == 3) thumbnails[i] = MediaStore.Video.Thumbnails.getThumbnail(
+                this.getContentResolver(), id.toLong(),
+                MediaStore.Video.Thumbnails.MINI_KIND, bmOptions
+            )
+            arrPath[i] = imagecursor.getString(dataColumnIndex)
+            try {
+                typeMedia!![i] = imagecursor.getInt(type)
+            }catch (e:NullPointerException){
+                Log.e("NullException<<","YES<<<")
+            }
+        }
+        Log.e("Size>>>", "${thumbnails.size}<<<")
+        var galleryAdapter = GalleryAdapter(this@MainActivity, thumbnails)
+        main_rv.adapter=galleryAdapter
+        main_loader.visibility=View.GONE
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_PERMISSIONS->{
+                for (i in 0 until  grantResults.size) {
+                    if (grantResults.size > 0 && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    getMedia();
+                } else {
+                    Toast.makeText(this, "The app was not allowed to read or write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sameep/galleryapp/adapters/GalleryAdapter.kt
@@ -0,0 +1,34 @@
+package com.sameep.galleryapp.adapters
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sameep.galleryapp.R
+import kotlinx.android.synthetic.main.rt_gallery.view.*
+
+class GalleryAdapter(val context: Context, var thumbnails: Array<Bitmap?>
+) : RecyclerView.Adapter<GalleryAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+       fun setData(position: Int){
+           itemView.rt_iv.setImageBitmap(thumbnails[position])
+       }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.rt_gallery, parent, false)
+        return MyViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+       return thumbnails.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.setData(position)
+    }
+}
